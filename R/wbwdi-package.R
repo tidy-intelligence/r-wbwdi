b56ba7f0a59d386633f8f613109df9574d3740df
Christoph Scheuch <19249734+christophscheuch@users.noreply.github.com>
2025-02-27 08:08:27 +0100

    Add %||% to imports

--- R/wbwdi-package.R
+++ R/wbwdi-package.R
@@ -8,6 +8,6 @@
 ## usethis namespace: start
 #' @importFrom purrr map map_df reduce
 #' @importFrom tidyr unnest nest pivot_wider unnest_wider unnest_longer
-#' @importFrom rlang parse_expr eval_tidy .data
+#' @importFrom rlang parse_expr eval_tidy .data %||%
 ## usethis namespace: end
 NULL
